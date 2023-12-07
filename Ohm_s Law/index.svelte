e4a6ff45dacda4560de48ee8aa1d6e43eebc0c66
Dirk Arnez <smalldirkalex@gmail.com>
2023-12-07 17:28:37 +0800

    - add Ohm_s Law/index.svelte

---
+++ Ohm_s Law/index.svelte
@@ -0,0 +1,25 @@
+<script>
+	$: i = 1;
+	$: r = 1;
+
+	// the `$:` means 're-run whenever these values change'
+	$: v = i * r;
+
+
+	function handleClickI() {
+		i += 1;
+	}
+
+	function handleClickR() {
+		r += 1;
+	}
+</script>
+
+<button on:click={handleClickI}>
+	Count: {v} = {i} * {r}
+</button>
+
+<button on:click={handleClickR}>
+	Count: {v} = {i} * {r}
+</button>
+
